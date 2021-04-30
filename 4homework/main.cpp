cd27afc6fb1c113e939b33683b80ba639b821d58
Arsen Gevorgyan <arseng767@gmail.com>
2021-04-30 18:35:55 +0400

    changed typename 'A' to 'T'

--- 4homework/main.cpp
+++ 4homework/main.cpp
@@ -1,22 +1,25 @@
 #include "ParallelScheduler.hpp"
 
-void foo(void* arg) {
+void foo(void* arg)
+{
     std::cout << "\nfoo called\n";
-    int* a = (int*)arg;
+    int* a = (int*) arg;
     std::cout << *a << ' ';
 }
 
 template<typename T>
-void print(const T& x){
-     std::cout << x << std::endl;
+void print(const T& x)
+{
+    std::cout << x << std::endl;
 
 }
 
-int main() {
+int main()
+{
     print("begin");
     ParallelScheduler scheduler(14);
     print("end");
-    for(int i = 1; i < 21; ++i) {
+    for (int i = 1; i<21; ++i) {
         print("\nin for loop, from main");
         scheduler.run(foo, &i);
     }
