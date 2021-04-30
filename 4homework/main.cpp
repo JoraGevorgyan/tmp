f7282d2118f956facc6ef8b7d6ad933e02af31ca
Arsen Gevorgyan <arseng767@gmail.com>
2021-04-30 12:12:50 +0400

    added print function

--- 4homework/main.cpp
+++ 4homework/main.cpp
@@ -6,10 +6,16 @@ void foo(void* arg) {
     std::cout << *a << ' ';
 }
 
+template<typename A>
+void print(const A& x){
+     std::cout << x << std::endl;
+
+}
+
 int main() {
     std::cout << "begin\n";
     ParallelScheduler scheduler(14);
-    std::cout << "end\n";
+    print("end");
     for(int i = 1; i < 21; ++i) {
         std::cout << "\nin for loop, from main\n";
         scheduler.run(foo, &i);
